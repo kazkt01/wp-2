8e0a2687a043c825a7d998a30ce6cba10d54159f
kazkt01 <91418688+kazkt01@users.noreply.github.com>
2024-10-29 03:07:53 +0900

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,13 @@
-import "../styles/page-styles/home.css";
-// import face from "../assets/videos/siro.mp4";
-// import faceLow from "../assets/videos/siro-low.mp4";
 import nobg from "../assets/videos/nobg01.mp4";
+import "../styles/page-styles/home.css";
 
 function Home() {
   return (
     <div>
       <section>
         <div className="hero-box">
-          {/* <video className="hero-video" autoPlay muted loop>
-            <source src="../src/assets/video/shibuya.mp4" />
-          </video>
-          <figcaption>
-            <small>*この動画は、Udemyの講座をもとにUE5で作成したものです</small>
-          </figcaption> */}
           <video className="idea-2" autoPlay playsInline muted loop>
-            <source src={nobg} type="videp/mp4" />
+            <source src={nobg} type="video/mp4" />
           </video>
           <h2>Hi! Im Kazuki Tachinana</h2>
           <h1>”Welcome to My Portfolio”</h1>
